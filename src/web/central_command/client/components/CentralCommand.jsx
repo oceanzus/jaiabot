827ccb8a1628f66404616858d9482b80666caac8
oceanzus <case@oceanz.org>
2022-08-19 00:29:01 -0400

    test

--- src/web/central_command/client/components/CentralCommand.jsx
+++ src/web/central_command/client/components/CentralCommand.jsx
@@ -867,17 +867,19 @@ export default class CentralCommand extends React.Component {
 		this.missionLayer = new OlVectorLayer()
 
 		// this.cacheTileLoad();
-		addToStore1('urlkey1', 'blob1').then((p) => {
-			console.log(p);
-		}).catch(() => {
 
-		});
 
 		getFromStore1('urlkey1').then((p) => {
 			console.log('key hit');
 			console.log(p);
 		}).catch(() => {
 			console.log('key miss');
+			addToStore1('urlkey1', 'blob1').then((p) => {
+				console.log('added urlkey1 to store');
+				console.log(p);
+			}).catch(() => {
+				console.log('urlkey1 already exists');
+			});
 		});
 
 		// demo1();
